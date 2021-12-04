9ccb60f3a38f04f1c63d5461888c0792b6db3c89
Lazaro1 <lazarolimasilva2@gmail.com>
2021-12-04 19:52:27 -0300

    first commit

--- src/components/main/styles.ts
+++ src/components/main/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  background-color: #06092b;
+  background-color: ${({ theme }) => theme.quina};
   color: #fff;
   width: 100%;
   height: 100%;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import Main from 'components/main'
+import { ThemeProvider } from 'styled-components'
 
 export default function Home() {
-  return <Main />
+  const theme = {
+    mega: '#6BEFA3',
+    quina: '#8666EF',
+    loto: '#DD7AC6',
+    lotoMania: '#FFAB64',
+    timeMania: '#5AAD7D',
+    sorte: '#BFAF83'
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
+  )
 }
